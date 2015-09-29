ce0ac1f44ab18c7a468ce000d9e21f221b547a75
wenbiao <wenbiao@enveesoft.com>
2015-09-29 18:30:33 +0800

    no message

---
+++ WXAPI/src/cn/s7k7k/sheep/Constants.java
@@ -0,0 +1,10 @@
+package cn.s7k7k.sheep;
+
+public class Constants
+{
+	public static String APP_ID = "wx77125009d79d3b29";
+	public static String SHARE_ICON_PATH = "";
+	public static String SHARE_TITLE = "";
+	public static String SHARE_DESC = "";
+	public static String SHARE_URL = "";
+}
